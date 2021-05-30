ef655da4de512844c9cfdfa67e76bdf3bb76e786
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-30 18:32:39 +0100

    feat(stream): use new stream

--- packages/actors/src/Journal/index.ts
+++ packages/actors/src/Journal/index.ts
@@ -2,8 +2,8 @@ import * as C from "@effect-ts/core/Case"
 import * as CH from "@effect-ts/core/Collections/Immutable/Chunk"
 import * as HM from "@effect-ts/core/Collections/Immutable/HashMap"
 import * as T from "@effect-ts/core/Effect"
+import * as S from "@effect-ts/core/Effect/Experimental/Stream"
 import * as REF from "@effect-ts/core/Effect/Ref"
-import * as S from "@effect-ts/core/Effect/Stream"
 import { pipe, tuple } from "@effect-ts/core/Function"
 import { tag } from "@effect-ts/core/Has"
 import * as O from "@effect-ts/core/Option"
@@ -20,7 +20,11 @@ export interface Journal<S, EV> {
   ): T.Effect<unknown, Throwable, void>
   getEntry(
     persistenceId: PersistenceId
-  ): T.Effect<unknown, Throwable, readonly [O.Option<S>, S.IO<Throwable, EV>]>
+  ): T.Effect<
+    unknown,
+    Throwable,
+    readonly [O.Option<S>, S.Stream<unknown, Throwable, EV>]
+  >
 }
 
 export interface JournalFactory {
@@ -58,7 +62,11 @@ export class InMemJournal<S, EV> implements Journal<S, EV> {
 
   getEntry(
     persistenceId: PersistenceId
-  ): T.Effect<unknown, Throwable, readonly [O.Option<S>, S.IO<Throwable, EV>]> {
+  ): T.Effect<
+    unknown,
+    Throwable,
+    readonly [O.Option<S>, S.Stream<unknown, Throwable, EV>]
+  > {
     return pipe(
       REF.get(this.journalRef),
       T.map((_) => O.getOrElse_(HM.get_(_, persistenceId), () => CH.empty<EV>())),
