b3043d1374402ef4a5a1b955acb61e762667e710
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-31 19:42:00 +0100

    feat(keeper): continue with client

--- packages/keeper/src/client.ts
+++ packages/keeper/src/client.ts
@@ -2,6 +2,7 @@ import { Tagged } from "@effect-ts/core/Case"
 import * as T from "@effect-ts/core/Effect"
 import * as L from "@effect-ts/core/Effect/Layer"
 import * as M from "@effect-ts/core/Effect/Managed"
+import * as P from "@effect-ts/core/Effect/Promise"
 import { tag } from "@effect-ts/core/Has"
 import type { _A } from "@effect-ts/system/Utils"
 import * as Z from "node-zookeeper-client"
@@ -18,6 +19,8 @@ export const KeeperClientSym = Symbol()
 export const makeKeeperClient = M.gen(function* (_) {
   const { connectionString, options } = yield* _(KeeperConfig)
 
+  const monitor = yield* _(P.make<ZooError, never>())
+
   const client = yield* _(
     T.effectAsync<unknown, ZooError, Z.Client>((cb) => {
       const cli = Z.createClient(connectionString, options)
@@ -31,6 +34,16 @@ export const makeKeeperClient = M.gen(function* (_) {
       })
 
       cli.connect()
+      cli.on("state", (s) => {
+        if (s.code === Z.State.DISCONNECTED.code) {
+          T.run(
+            P.fail_(
+              monitor,
+              new ZooError({ op: "DISCONNECTED", message: JSON.stringify(s) })
+            )
+          )
+        }
+      })
     })["|>"](M.make((cli) => T.succeedWith(() => cli.close())))
   )
 
@@ -82,16 +95,59 @@ export const makeKeeperClient = M.gen(function* (_) {
     })
   }
 
+  function waitDelete(path: string) {
+    return T.effectAsync<unknown, ZooError, string>((cb) => {
+      client.exists(
+        path,
+        (ev) => {
+          if (ev.name === "NODE_DELETED") {
+            cb(T.succeed(ev.path))
+          }
+        },
+        (e, p) => {
+          if (e) {
+            cb(T.fail(new ZooError({ op: "CREATE", message: JSON.stringify(e) })))
+          } else {
+            if (p == null) {
+              cb(
+                T.fail(
+                  new ZooError({ op: "WAIT_DELETE", message: "path does not exist" })
+                )
+              )
+            }
+          }
+        }
+      )
+    })
+  }
+
+  function remove(path: string) {
+    return T.effectAsync<unknown, ZooError, void>((cb) => {
+      client.remove(path, (e) => {
+        if (e) {
+          cb(T.fail(new ZooError({ op: "REMOVE", message: JSON.stringify(e) })))
+        } else {
+          cb(T.unit)
+        }
+      })
+    })
+  }
+
   return {
     [KeeperClientSym]: KeeperClientSym,
     client,
     create,
     createWithData,
     mkdir,
-    mkdirWithData
+    mkdirWithData,
+    monitor,
+    waitDelete,
+    remove
   } as const
 })
 
 export interface KeeperClient extends _A<typeof makeKeeperClient> {}
 export const KeeperClient = tag<KeeperClient>()
 export const LiveKeeperClient = L.fromManaged(KeeperClient)(makeKeeperClient)
+
+export { Event } from "node-zookeeper-client"
--- packages/keeper/test/index.test.ts
+++ packages/keeper/test/index.test.ts
@@ -1,4 +1,5 @@
 import * as T from "@effect-ts/core/Effect"
+import * as F from "@effect-ts/core/Effect/Fiber"
 import * as J from "@effect-ts/jest/Test"
 import { pipe } from "@effect-ts/system/Function"
 
@@ -10,6 +11,8 @@ describe("Zookeeper", () => {
     J.runtime((TestEnv) => TestEnv[">+>"](TestKeeperConfig[">>>"](Z.LiveKeeperClient)))
   )
 
+  jest.setTimeout
+
   it("dummy", () =>
     T.gen(function* (_) {
       const client = yield* _(Z.KeeperClient)
@@ -17,11 +20,18 @@ describe("Zookeeper", () => {
       expect(yield* _(client.mkdir("/demo"))).toEqual("/demo")
       expect(yield* _(client.create("/demo/ok"))).toEqual("/demo/ok")
 
+      const collectFiber = yield* _(pipe(client.waitDelete("/demo/ok"), T.fork))
+
       expect(yield* _(client.mkdirWithData("/demo-2", Buffer.from("yeah")))).toEqual(
         "/demo-2"
       )
       expect(
         yield* _(client.createWithData("/demo-2/ok", Buffer.from("yeah yeah")))
       ).toEqual("/demo-2/ok")
+
+      yield* _(client.remove("/demo/ok"))
+      yield* _(client.remove("/demo"))
+
+      expect(yield* _(F.join(collectFiber))).toEqual("/demo/ok")
     }))
 })
