bd9f83bf8e913eb643ac94b4ef70048c67da28ae
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-03 00:06:40 +0100

    refactor(actors): remove leftover

--- packages/actors/src/Persistent/index.ts
+++ packages/actors/src/Persistent/index.ts
@@ -50,16 +50,6 @@ export interface StateStorageAdapter {
 
 export const StateStorageAdapter = tag<StateStorageAdapter>()
 
-export function hashStringToInt(str: string): number {
-  let hash = 5381
-  let i = -1
-  while (i < str.length - 1) {
-    i += 1
-    hash = (hash * 33) ^ str.charCodeAt(i)
-  }
-  return hash >>> 0
-}
-
 export const LiveStateStorageAdapter = L.fromManaged(StateStorageAdapter)(
   M.gen(function* (_) {
     const cli = yield* _(PG.PG)
